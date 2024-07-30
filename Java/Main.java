8c41d36e2206a6c5b7e5b41af275095a29d5e661
BloodKrimson <brandonangos05@gmail.com>
2024-07-29 22:09:29 -0400

    cm

--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,13 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
-        HelloWorld hw = new HelloWorld();
-        hw.greet();
+        float mult = (float) 2.205;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number of kilos: ");
+        float kg = s.nextFloat();
+        float lbs = kg * mult;
+        System.out.println(kg + " kg is " + lbs + " pounds");
 
-        Conversion test = new Conversion(2);
-        System.out.println(test.getPounds());
-        test.setK(42);
-        System.out.println(test.getPounds());
     }
 }
