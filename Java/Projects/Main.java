687cbbe55c9e363fd28a684599caabc3fceda368
BloodKrimson <brandonangos05@gmail.com>
2025-01-07 23:31:56 -0500

    edit

--- Java/Projects/Main.java
+++ Java/Projects/Main.java
@@ -1,14 +1,19 @@
 import java.util.*;
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args){
-        String name;
-        CreateFile f = new CreateFile();
-        System.out.print(f.getName());
-        Scanner s = new Scanner(System.in);  
-        System.out.print("\nWhat is your name? " );
-        name = s.nextLine();
-        System.out.print(name);
+        Path currRelativePath = Paths.get("");
+        String currAbsolutePathString = currRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is - " + currAbsolutePathString);
+        // String name;
+        // CreateFile f = new CreateFile();
+        // System.out.print(f.getName());
+        // Scanner s = new Scanner(System.in);  
+        // System.out.print("\nWhat is your name? " );
+        // name = s.nextLine();
+        // System.out.print(name);
         
 
     }
